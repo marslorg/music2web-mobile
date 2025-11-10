4184c469a03f42432dd82601edee3ec2dfd058c0
marcellinke <Marcel.Linke@gmail.com>
2025-11-10 21:04:56 +0100

    chore: Setup MAUI project

---
+++ Music2Web.de/Music2Web/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Music2Web
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Music2Web.de/Music2Web/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Music2Web
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
